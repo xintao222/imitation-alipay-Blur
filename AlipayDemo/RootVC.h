502d7f33caf9c600a902d3a1e036f1e3d6fe1378
lijianfei <f.li@qunar.com>
2014-12-22 15:23:54 +0800

    first commit

---
+++ AlipayDemo/RootVC.h
@@ -0,0 +1,13 @@
+//
+//  RootVC.h
+//  AlipayDemo
+//
+//  Created by f.li on 14/12/22.
+//  Copyright (c) 2014年 Qunar.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootVC : UIViewController
+
+@end
---
+++ AlipayDemo/RootVC.m
@@ -0,0 +1,37 @@
+//
+//  RootVC.m
+//  AlipayDemo
+//
+//  Created by f.li on 14/12/22.
+//  Copyright (c) 2014年 Qunar.com. All rights reserved.
+//
+
+#import "RootVC.h"
+
+@interface RootVC ()
+
+@end
+
+@implementation RootVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
