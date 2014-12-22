d23cde5be7a2ec44dd5e93d44e8e6ea0442d26f5
lijianfei <f.li@qunar.com>
2014-12-22 14:58:26 +0800

    Initial Commit

---
+++ AlipayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlipayDemo
+//
+//  Created by f.li on 14/12/22.
+//  Copyright (c) 2014年 Qunar.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
